e85fa37e10888fa50a981951e76f0eabc1a43f30
petitejess <jessica.gozali@outlook.com.au>
2021-08-08 03:22:21 +1000

    Complete autocomplete in profile page

---
+++ app/controllers/suburbs_controller.rb
@@ -0,0 +1,38 @@
+require 'faraday'
+require 'json'
+
+class SuburbsController < ApplicationController
+  before_action :set_suburbs, only: [:index]
+
+  def index
+    respond_to do |format|
+      format.json { render json: @suburbs.to_json }
+    end
+  end
+
+  private
+
+  def set_suburbs
+    @suburbs = []
+
+    # Get the possible suburbs from Postcode API based on user input passed as params :term
+    if params[:term]
+      url = 'https://v0.postcodeapi.com.au/suburbs.json'
+      # Use faraday gem to handle the HTTP request
+      response = Faraday.get(url, {q: params[:term]}, {'Accept' => 'application/json; indent=4'})
+      results = JSON.parse(response.body)
+      results.each do |result|
+        # Not to include PO Boxes (https://en.wikipedia.org/wiki/Postcodes_in_Australia)
+        if result["postcode"].in?([*1..9999] - [*200..299, *900..999, *1000..1999, *5800..5999, *6800..6999, *7800..7999, *8000..8999, *9000..9999])
+          # Collect valid postcodes (format following json from Postcode API), postcode int to str and format it to allow 0 at front
+          @suburbs << {"suburb": result["name"], "state": result["state"]["abbreviation"], "postcode": result["postcode"].to_s.rjust(4, "0")}
+        end
+      end
+    else
+      results = Suburb.all
+      results.each do |result|
+        @suburbs << {"suburb": result["name"], "state": result.postcode.state.name, "postcode": result.postcode.number.to_s.rjust(4, "0")}
+      end
+    end
+  end
+end
